5b5d09b08c4475820b546beeddf62c368fef5bb4
[zzuluagarend] <zzuluagar@gmail.com>
2020-07-30 12:47:26 +0200

    instructions

--- builder.js
+++ builder.js
@@ -14,7 +14,7 @@ var surveyJSON = {
     {
      "type": "html",
      "name": "introduction",
-     "html": "<!DOCTYPE html>\n<html>\n<body>\n\n<h1><center>Welcome!</center></h1>\n<br />\n<br />\n<p>Thank you for taking part in this experiment! The data you will provide here will be kept anonymous and only shared with the researchers supervising this master thesis. Your participation in this experiment is voluntary. You may quit at any moment without negative consequences.\n<br />\n<br />\nThe experiment will take around 30 minutes. Please read the instructions carefully and answer the questions fully.</p>\n<br />\n<br />\n</body>\n</html>\n"
+     "html": "<!DOCTYPE html>\n<html>\n<body>\n\n<h1><center>Welcome!</center></h1>\n<br />\n<br />\n<p>Thank you for taking part in this experiment! The data you will provide here will be kept anonymous and only shared with the researchers supervising this master thesis. Your participation in this experiment is voluntary. You may quit at any moment without negative consequences.\n<br />\n<br />\nThe experiment will take around 30 minutes during which <b>I will ask you to neither leave the window or search in your phone</b>. Please read the instructions carefully and answer the questions fully.</p>\n<br />\n<br />\n</body>\n</html>\n"
     }
    ]
   },
